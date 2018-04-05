c81c8966881b9c92278df5ee1b300b9825de428f
Rommel Fernando <rommelfernando@Rommels-MacBook-Pro.local>
2018-04-04 20:51:25 -0700

    added article Schema

---
+++ models/article.js
@@ -0,0 +1,27 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var ArticleSchema = new Schema ({
+    title: {
+        type: String,
+        required: true
+    },
+    
+    description: {
+        type: String,
+        required: true
+    },
+    
+    link: {
+        type: String,
+        required: true
+    },
+
+    comments: {
+        type: String
+    }
+});
+
+var Article = mongoose.model("Article", ArticleSchema);
+
+module.exports = Article;
