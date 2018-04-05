95ded540a1e314282d0bbe1e25b05245de9e0675
Rommel Fernando <rommelfernando@Rommels-MacBook-Pro.local>
2018-04-04 18:50:06 -0700

    updated server file and added models

---
+++ models/index.js
@@ -0,0 +1,4 @@
+// Exporting an object containing article/notes models
+module.exports = {
+    Article: require("./article"),
+  };
---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+//scraping tools 
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+//requiring all models
+var db = require("./models")
+var PORT = process.env.PORT || 3000;
+
+//initialize Express
+var app = express();
+
+//morgan logger for logging request
+app.use(logger("dev"));
+
+//use body-parser for form submissions
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//making "public" a static directory
+app.use(express.static("public"));
+
+//connect to mongoose db
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/nbaFantasyBasketballNews";
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, {
+    useMongoClient: true
+});
+
+//setting handlebars
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+// Start the server
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+  });
+  
